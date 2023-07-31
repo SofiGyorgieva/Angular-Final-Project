2d6f333d68d5e813db6f2d8ec586b4b997c2824b
sofia_g <sofi_gorgieva@hotmail.com>
2023-07-31 22:59:07 +0300

    Home page design, and Recipes cards design

--- examProject/src/app/home/home.component.ts
+++ examProject/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../core/header/header.component';
+import { FooterComponent } from '../core/footer/footer.component';
 
 @Component({
   selector: 'app-home',
